2c60f61db8ccd208395175309499e4544605ce10
developerbrm <developer.brm@gmail.com>
2022-03-15 00:25:33 +0530

    - reverseString shorter version

--- js/reverseString.js
+++ js/reverseString.js
@@ -1,19 +1,4 @@
-const reverseString = (arr) => {
-	let i = 0,
-		j = arr.length - 1;
+const reverseString = (arr) => arr.reduce((acc, val) => [val, ...acc], []);
 
-	while (i < Math.ceil(arr.length / 2)) {
-		const lastChar = arr.at(j);
-		const firstChar = arr.at(i);
-
-		arr.splice(i, 1, lastChar);
-		arr.splice(j, 1, firstChar);
-
-		i++;
-		j--;
-	}
-
-	return arr;
-};
-
-console.log(reverseString(["h", "e", "l", "l", "o"]));
+// console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["a", "b", "c", "d", "e"]));
