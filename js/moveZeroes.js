6193a41f04fce7846d243962d9ab14ae35cd29c5
developerbrm <developer.brm@gmail.com>
2022-03-23 12:34:08 +0530

    - long solution also added for `moveZeroes`

--- js/moveZeroes.js
+++ js/moveZeroes.js
@@ -1,4 +1,20 @@
-const moveZeroes = (nums) => nums.sort((a, b) => (a === 0 ? 1 : 0));
+// const moveZeroes = (nums) => nums.sort((a, b) => (a === 0 ? 1 : 0));
+const moveZeroes = (nums) => {
+	if (nums.length < 2) return nums;
+	let zerosCount = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums.at(i) === 0) {
+			nums.splice(i, 1);
+			zerosCount++;
+			i--;
+		}
+	}
+
+	nums.push(...Array(zerosCount).fill(0));
+
+	return nums;
+};
 
 [[0, 1, 0, 3, 12], [0], [0, 0, 1]].forEach((arr) => {
 	// [[0, 0, 1]].forEach((arr) => {
