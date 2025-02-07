313c50dc655c78bcb7ba4071949a55f79bd7a478
developerbrm <developerbrm@gmail.com>
2025-02-07 18:50:28 +0530

    - `containsDuplicate`

---
+++ js/containsDuplicate.ts
@@ -0,0 +1,6 @@
+const containsDuplicate = (nums: number[]): boolean =>
+  new Set(nums).size !== nums.length;
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
