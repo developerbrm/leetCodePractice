e244a1fec276fb43f6b10d72b8a74bc816f4527a
developerbrm <developerbrm@gmail.com>
2025-02-12 23:00:01 +0530

    - new method

--- js/canConstruct.ts
+++ js/canConstruct.ts
@@ -1,15 +1,41 @@
-import { makeHashMapFromString } from './utilities';
+// import { makeHashMapFromString } from './utilities';
+
+// function canConstruct(ransomNote: string, magazine: string): boolean {
+//   if (magazine.length < ransomNote.length) return false;
+//   if (magazine === ransomNote) return true;
+
+//   const magazineMap = makeHashMapFromString(magazine);
+//   const ransomNoteMap = makeHashMapFromString(ransomNote);
+
+//   for (let [key, value] of ransomNoteMap) {
+//     const magazineValue = magazineMap.get(key);
+
+//     if (!magazineValue || magazineValue < value) return false;
+//   }
+
+//   return true;
+// }
 
 function canConstruct(ransomNote: string, magazine: string): boolean {
-  const magazineMap = makeHashMapFromString(magazine);
-  const ransomNoteMap = makeHashMapFromString(ransomNote);
+  if (magazine.length < ransomNote.length) return false;
+  if (magazine === ransomNote) return true;
 
-  for (let [key, value] of ransomNoteMap) {
-    if (magazineMap.get(key) < value) return false;
+  const magazineArr = magazine.split('');
+
+  for (const noteItem of ransomNote) {
+    const magazineItemIndex = magazineArr.indexOf(noteItem);
+
+    if (magazineItemIndex === -1) return false;
+
+    magazineArr[magazineItemIndex] = '-';
   }
 
   return true;
 }
 
+console.log(canConstruct('a', 'b'));
 console.log(canConstruct('aab', 'baa'));
-console.log(canConstruct('bg', 'efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj'));
+console.log(
+  canConstruct('bg', 'efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj')
+);
+//
